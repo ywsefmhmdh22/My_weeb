c6fc86564968bafd1cf226396b3c2733ab224fb1
unknown <ywsfalmajk7@gmail.comgit config --global user.name>
2025-03-14 16:03:09 +0200

    إضافة التعديلات الأخيرة

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export", // يسمح بالتصدير إلى ملفات HTML ثابتة
+  distDir: "out", // تحديد مجلد الإخراج
+  images: {
+    unoptimized: true, // تعطيل تحسين الصور عند التصدير
+  },
 };
 
 export default nextConfig;
